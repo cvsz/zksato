0b3b26e003a0cf39c1f4ddc05edf040301f45367
cvsz <seaza@msn.com>
2026-08-09 05:45:35 +0700

    test: import OpenAPI contract from installed package

--- tests/test_openapi_contract.py
+++ tests/test_openapi_contract.py
@@ -1,5 +1,5 @@
 from zksato.api import app
-from scripts.openapi_contract import validate_schema
+from zksato.openapi_contract import validate_schema
 
 
 def test_openapi_contract_preserves_critical_control_boundaries() -> None:
